93597f74a3ee02e5b900878020de0acb6cb3548e
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-31 15:34:05 +0900

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,43 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors:{
+        primary: '#008A53',
+        primaryDark:'#005538',
+        secondary:'#0E4238',
+
+        none: 'transparent',
+        white: '#ffffff',
+
+        black100: '#333333',
+        black200: '#222222',
+        black300: '#181818',
+
+        grey000: '#fafafa',
+        grey100: '#f6f6f6',
+        grey200: '#eeeeee',
+        grey300: '#e2e2e2',
+        grey400: '#cccccc',
+        grey500: '#aaaaaa',
+        grey600: '#9DA2A7',
+        grey700: '#888888',
+        grey800: '#797979',
+        grey900: '#555555',
+
+        red: '#ED5C5C',
+        redBg: '#FFF2F2',
+
+        blue: '#4788f4',
+        blueBg: '#EEF4FD',
+
+        ground100: '#f8f9fc',
+        ground200: '#f5f7fc',
+
+        green: '#00AB83',
+      }
     },
   },
   plugins: [],
