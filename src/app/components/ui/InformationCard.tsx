220b9e0e06e17bdc7ace04c2bd8c1d3e872c7650
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-10-31 20:23:53 +0900

    Feat(공통): 공통 Button 추가

---
+++ src/app/components/ui/InformationCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function InformationCard() {
+    return (
+
+        <div className="w-64 p-4 m-auto bg-white shadow-lg rounded-2xl dark:bg-gray-800">
+            <div className="w-full h-full text-center">
+                <div className="flex flex-col justify-between h-full">
+                    <svg className="w-12 h-12 m-auto mt-4 text-green-500" stroke="currentColor" fill="none"
+                         viewBox="0 0 24 24">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7">
+                        </path>
+                    </svg>
+                    <p className="px-6 py-2 text-gray-600 dark:text-gray-100 text-md">
+                        User
+                        <span className="font-bold text-gray-800 dark:text-white">
+                    23722873
+                </span>
+                        has been deleted form database.
+                    </p>
+                    <div className="flex items-center justify-between w-full gap-4 mt-8">
+                        <button type="button"
+                                className="w-full py-2 px-4 bg-primary hover:bg-primaryDark text-white transition ease-in duration-50 text-center text-base font-semibold shadow-md rounded-lg ">
+                            확인
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default InformationCard;
