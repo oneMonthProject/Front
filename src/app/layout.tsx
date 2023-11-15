9514aed30f7ccdf412d7a622c3491417b0d0695e
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-11-15 22:43:54 +0900

    refactor(global.css): 기본 폰트, 텍스트 색상 base 수준으로 레이어에 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
 import './globals.css'
 import Providers from "@/app/Providers";
 import React from "react";
 
-const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
     title: 'trustcrews',
