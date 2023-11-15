27295b00c08e5bc8bc82746fbda4df1604aa8a55
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-11-15 15:32:28 +0900

    Chore(tailwind.config.ts) : 반응형 breakpoint추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,50 +1,52 @@
-import type { Config } from 'tailwindcss';
+import type {Config} from 'tailwindcss';
+
 require('tailwindcss/plugin');
 
 const config: Config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors:{
-        primary:'#008A74',
-        secondary:'#005345',
+    content: ["./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#008A74',
+                secondary: '#005345',
 
-        none: 'transparent',
-        white: '#ffffff',
+                none: 'transparent',
+                white: '#ffffff',
 
-        black100: '#333333', // 선택된 nav, 일반 폰트 색상
-        black200: '#222222',
-        black300: '#181818',
+                black100: '#333333', // 선택된 nav, 일반 폰트 색상
+                black200: '#222222',
+                black300: '#181818',
 
-        grey000: '#fafafa',
-        grey100: '#f6f6f6',
-        grey200: '#eeeeee', // 취소버튼, 신뢰점수 - 버튼
-        grey300: '#e2e2e2',
-        grey400: '#cccccc',
-        grey500: '#aaaaaa',
-        grey600: '#9DA2A7',
-        greyUnselect:'#868E96', // 선택안된 nav
-        grey700: '#888888',
-        grey800: '#797979',
-        grey900: '#555555',
+                grey000: '#fafafa',
+                grey100: '#f6f6f6',
+                grey200: '#eeeeee', // 취소버튼, 신뢰점수 - 버튼
+                grey300: '#e2e2e2',
+                grey400: '#cccccc',
+                grey500: '#aaaaaa',
+                grey600: '#9DA2A7',
+                greyUnselect: '#868E96', // 선택안된 nav
+                grey700: '#888888',
+                grey800: '#797979',
+                grey900: '#555555',
 
-        warn: '#ED5C5C',
+                warn: '#ED5C5C',
 
-        ground100: '#f8f9fc',
-        ground200: '#f5f7fc',
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-            'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      }
+                ground100: '#f8f9fc',
+                ground200: '#f5f7fc',
+            },
+            screens: {
+                'mobile': {'max': '833px'},
+                'tablet': '834px',
+                'pc': '1440px'
+            }
+        },
     },
-  },
-  plugins: [
-    require('tailwindcss/nesting'),
-    require('tailwindcss'),
-    require('autoprefixer')
-  ],
+    plugins: [
+        require('tailwindcss/nesting'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('@tailwindcss/forms')
+    ],
 
 }
 export default config
