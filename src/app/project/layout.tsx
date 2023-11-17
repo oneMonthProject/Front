4db8f4f5521ee9166fd18750c51aa1b3f9320c1d
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-11-17 17:49:54 +0900

    Feat: 프로젝트 상세페이지 layout에 nav탭 추가

--- src/app/project/layout.tsx
+++ src/app/project/layout.tsx
@@ -1,6 +1,7 @@
 import React, {ReactNode} from 'react';
 import BackNav from "@/components/ui/BackNav";
 import ProjectInfo from "@/components/project/layout/ProjectInfo";
+import ProjectNavTab from "@/components/project/layout/ProjectNavTab";
 
 // Todo - 홈 버튼, 상단 프로젝트 정보, 업무/크루정보/알림/투표 nav탭 // 후에 projectid 받아서 렌더링해야함.
 interface ProjectLayoutProps {
@@ -14,6 +15,7 @@ function ProjectLayout({children}: ProjectLayoutProps) {
                 <BackNav to='/'/>
             </section>
             <ProjectInfo/>
+            <ProjectNavTab/>
             {children}
         </section>
     );
