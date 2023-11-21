599dc757b19586210a21211ee7b08a55cb5dc29b
moa1128 <moa971128@gmail.com>
2023-11-21 15:58:17 +0900

    fix: login, register link hidden

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,9 +5,9 @@ import LoginForm from '@/components/user/login/LoginForm';
 function LoginPage() {
   return (
     <div className="max-w-[380px] mobile:max-w-[300px] m-auto">
-    <h1 className='text-3xl mobile:text-2xl font-semibold w-full text-center mb-10 mt-40 mobile:mt-20'>로그인</h1>
+    <h1 className='text-3xl mobile:text-2xl font-semibold w-full text-center mb-10 mt-44 mobile:mt-32'>로그인</h1>
     <LoginForm />
-    <div className="text-center text-sm mobile:text-xs mt-3 mb-10">
+    <div className="text-center text-sm mt-3 mb-10 mobile:hidden">
       이미 회원이신가요?
       <Link href='/register'>
         <span className="ml-1 underline text-blue-600">회원가입</span>
