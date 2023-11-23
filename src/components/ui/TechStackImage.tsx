19e3600c2ffa7b474d2dc4aeb6a8b3e9f3b97014
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-11-23 15:52:35 +0900

    feat(공통): TechStackImage.tsx > 이미지 경로 env 파일통해서 찾도록 수정

--- src/components/ui/TechStackImage.tsx
+++ src/components/ui/TechStackImage.tsx
@@ -4,10 +4,9 @@ import Image from "next/image";
 function TechStackImage({stackName}:{stackName:string}) {
     return (
         <Image
-            src={`/images/${stackName.toLowerCase()}.svg`}
+            src={`${process.env.NEXT_PUBLIC_URL}/images/tech/${stackName.toLowerCase()}.svg`}
             alt={stackName}
-            width={30}
-            height={30}
+            fill
         />
     );
 }
