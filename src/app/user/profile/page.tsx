2fe1c0de4fdb9c86293399c6205e69724f67b822
moa1128 <moa971128@gmail.com>
2023-11-26 15:43:45 +0900

    feat: modify UserHistory

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -8,7 +8,10 @@ function ProfilePage() {
       <div className='rounded-lg border-2 border-gray-200 bg-white mt-3 mobile:mt-2'>
         <ProfileCard />
       </div>
-      <UserHistory />
+      <div className='p-3 mobile:p-0 mobile:pt-3 mt-2 space-y-5'>
+        <span>사용자 프로젝트 이력</span>
+        <UserHistory />
+      </div>
     </div>
   );
 }
