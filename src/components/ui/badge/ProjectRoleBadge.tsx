06bede55cab6fe793b8d0c4fef8ce95115171d65
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-11-28 00:31:11 +0900

    Fix: 프로젝트 권한 뱃지 디자인 버그 수정

--- src/components/ui/badge/ProjectRoleBadge.tsx
+++ src/components/ui/badge/ProjectRoleBadge.tsx
@@ -5,13 +5,13 @@ import {BadgeProps} from "@/utils/type";
 function ProjectRoleBadge({size = '', text = ''}: BadgeProps) {
     const {textSize, px, py} = makeBadgeSize(size);
 
-    const bgColor = text === '매니저' ? '#FF513A' : '#FFF9CF';
-    const textColor = text === '매니저' ? '#FFFFFF' : '#7B5C03';
+    const bgColor = text === '매니저' ? 'bg-[#FF513A]' : 'bg-[#FFF9CF]';
+    const textColor = text === '매니저' ? 'text-[#FFFFFF]' : 'text[#7B5C03]';
 
     if(text === '구성원') return null;
     return (
         <span
-            className={`inline-flex items-center rounded-full bg-[${bgColor}] text-[${textColor}] font-medium ${textSize} ${px} ${py}`}>
+            className={`inline-flex items-center rounded-full ${bgColor} ${textColor} font-medium ${textSize} ${px} ${py}`}>
             {text}
         </span>
     );
