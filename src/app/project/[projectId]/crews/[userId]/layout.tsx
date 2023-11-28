a57eb6eace6e0254e0274819daca566ebd12175a
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-11-28 16:16:46 +0900

    feat: project 동적 라우트 추가 & 업무/크루/알림/프로젝트 설정 라우트 경로 이동

--- src/app/project/[projectId]/crews/[userId]/layout.tsx
+++ src/app/project/[projectId]/crews/[userId]/layout.tsx
@@ -2,14 +2,21 @@ import React, {ReactNode} from 'react';
 import Button from "@/components/ui/Button";
 import Link from "next/link";
 
-function ProjectCrewDetailPageLayout({children}: { children: ReactNode }) {
+interface ProjectCrewDetailPageLayoutProps {
+    children: ReactNode;
+    params:{
+        projectId:string;
+    }
+}
+
+function ProjectCrewDetailPageLayout({children, params}: ProjectCrewDetailPageLayoutProps) {
     return (
         <section className='w-full flex flex-col items-center'>
             <section className='w-full px-1'>
                 {children}
             </section>
             <section className='mt-7'>
-                <Link href='/project/crews'><Button size='xl' theme='primary-hollow'>크루 목록</Button></Link>
+                <Link href={`/project/${params.projectId}/crews`}><Button size='xl' theme='primary-hollow'>크루 목록</Button></Link>
             </section>
         </section>
     );
