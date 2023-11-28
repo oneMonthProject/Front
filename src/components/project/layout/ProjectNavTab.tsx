760c0d6a363eedb6342b49e39ec880e787482a3e
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-11-28 20:31:20 +0900

    fix: ProjectNavTabStateStore 수정
    1. 현재 projectId에 따라 navTab link 달라지도록 수정
    2. 현재 navTab 아톰으로 상태관리

--- src/components/project/layout/ProjectNavTab.tsx
+++ src/components/project/layout/ProjectNavTab.tsx
@@ -2,8 +2,7 @@
 import React, {MouseEvent} from 'react';
 import Link from "next/link";
 import {useRecoilState, useRecoilValue} from "recoil";
-import {currentProjectNavTabSelector, projectNavTabState} from "@/store/project/ProjectNavTabStateStore";
-import {ProjectNavTabItem} from "@/utils/type";
+import {currentProjectNavTab, projectNavTabSelector} from "@/store/project/ProjectNavTabStateStore";
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
@@ -11,48 +10,39 @@ function classNames(...classes: string[]) {
 
 
 export default function ProjectNavTab() {
-    const [projectNavTabs, setProjectNavTabs] = useRecoilState(projectNavTabState);
-    const currentProjectNavTab = useRecoilValue(currentProjectNavTabSelector);
+    const projectNavTabs = useRecoilValue(projectNavTabSelector);
+    const [currentNavTab, setCurrentNavTab] = useRecoilState(currentProjectNavTab);
 
 
     function onClickHandler({target}: MouseEvent<HTMLAnchorElement>) {
-        const updatedProjectNavTabs: ProjectNavTabItem[] = [];
-
-        [...projectNavTabs].forEach((v) => {
-            updatedProjectNavTabs.push({
-                ...v,
-                current: v.href === (target as HTMLAnchorElement).dataset.pathname
-            });
-        });
-
-        setProjectNavTabs(updatedProjectNavTabs);
+        setCurrentNavTab((target as HTMLAnchorElement).dataset.pathname!);
     }
 
 
     return (
-            <div className='tablet:my-[3.9rem] mobile:my-[3rem]'>
-                <div className="border-b-[3px] border-grey300">
-                    <nav className="-mb-px flex tablet:space-x-10 mobile:justify-between" aria-label="Tabs">
-                        {projectNavTabs.map((tab) => (
-                            <Link
-                                key={tab.name}
-                                href={tab.href}
-                                className={classNames(
-                                    tab.href === currentProjectNavTab.href
-                                        ? 'border-secondary text-secondary'
-                                        : 'border-transparent text-gray-500 hover:border-gray-200 hover:text-gray-700',
-                                    'flex whitespace-nowrap border-b-[3px] -mb-[1.8px] py-4 px-1 mobile:px-4 pc:text-[2rem] tablet:text-[1.5rem] mobile:text-lg font-semibold'
-                                )}
-                                aria-current={tab.href === currentProjectNavTab.href ? 'page' : undefined}
-                                onClick={onClickHandler}
-                                data-pathname={tab.href}
-                                replace
-                            >
-                                {tab.name}
-                            </Link>
-                        ))}
-                    </nav>
-                </div>
+        <div className='tablet:my-[3.9rem] mobile:my-[3rem]'>
+            <div className="border-b-[3px] border-grey300">
+                <nav className="-mb-px flex tablet:space-x-10 mobile:justify-between" aria-label="Tabs">
+                    {projectNavTabs.map((tab) => (
+                        <Link
+                            key={tab.name}
+                            href={tab.href}
+                            className={classNames(
+                                tab.href === currentNavTab
+                                    ? 'border-secondary text-secondary'
+                                    : 'border-transparent text-gray-500 hover:border-gray-200 hover:text-gray-700',
+                                'flex whitespace-nowrap border-b-[3px] -mb-[1.8px] py-4 px-1 mobile:px-4 pc:text-[2rem] tablet:text-[1.5rem] mobile:text-lg font-semibold'
+                            )}
+                            aria-current={tab.href === currentNavTab ? 'page' : undefined}
+                            onClick={onClickHandler}
+                            data-pathname={tab.href}
+                            replace
+                        >
+                            {tab.name}
+                        </Link>
+                    ))}
+                </nav>
             </div>
+        </div>
     )
 }
