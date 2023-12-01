999395ce56d8b09df89f06ab5d83f5c740ad4388
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-12-01 16:29:09 +0900

    feat(공통): CommonPagination추가

---
+++ src/components/ui/CommonPagination.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import Pagination from "react-js-pagination";
+
+function CommonPagination() {
+    const [activePage, setActivePage] = useState(1);
+
+    function handlePageChange(pageNumber:number) {
+        setActivePage(pageNumber);
+    }
+
+
+    return (
+        <div className='mt-12 mb-10 mobile:max-w-[100px] mobile:mx-auto'>
+            <div className='customPagination'>
+                <Pagination
+                    activePage={activePage}
+                    itemsCountPerPage={10}
+                    totalItemsCount={450}
+                    pageRangeDisplayed={5}
+                    onChange={handlePageChange}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default CommonPagination;
