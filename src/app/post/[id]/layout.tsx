24177165d73e5bbf42a8bf8735e5255c0e2a0dd3
moa1128 <moa971128@gmail.com>
2023-12-01 16:13:17 +0900

    refactor: modify post detail page

---
+++ src/app/post/[id]/layout.tsx
@@ -0,0 +1,21 @@
+'use client';
+import React, { ReactNode } from 'react';
+import BackNav from '@/components/ui/BackNav';
+
+interface PostLayoutProps {
+  children: ReactNode;
+}
+
+function PostLayout({ children }: PostLayoutProps) {
+
+  return (
+    <section className='flex-col justify-center mx-auto tablet:pt-[1.5rem] px-[4rem] tablet:px[3rem] mobile:px-[0.5rem] pb-[1rem]'>
+      <section className='w-fit cursor-pointer'>
+        <BackNav to='/' />
+      </section>
+      {children}
+    </section>
+  );
+}
+
+export default PostLayout;
