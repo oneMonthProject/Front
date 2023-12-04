f31bc0693c8e2339a93f1db737189678c28919f8
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-12-04 23:25:36 +0900

    feat: react-query 세팅
    1. @tanstack/react-query, @tanstack/react-query-devtools 설치
    2. 루트 레이아웃 Provider에 react-query provider 추가

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,11 +1,18 @@
 'use client';
-import React, {ReactNode} from 'react';
-import { RecoilRoot } from 'recoil';
+import React, {ReactNode, useState} from 'react';
+import {RecoilRoot} from 'recoil';
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
-function Providers({children}:{children:ReactNode}) {
+function Providers({children}: { children: ReactNode }) {
+    const [queryClient] = useState(() => new QueryClient());
     return (
         <RecoilRoot>
-            {children}
+            <QueryClientProvider client={queryClient}>
+                <ReactQueryDevtools initialIsOpen={false}/>
+                {children}
+            </QueryClientProvider>
         </RecoilRoot>
     );
 }
