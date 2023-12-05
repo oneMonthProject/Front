a188e069e3d8044f181f508175a3035764edb715
moa1128 <moa971128@gmail.com>
2023-12-05 19:13:31 +0900

    feat: add project setting form

--- src/app/project/setting/page.tsx
+++ src/app/project/setting/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import ProjectSettingForm from '@/components/project/setting/ProjectSettingForm';
 
 function SettingPage() {
-    return (
-        <div>setting</div>
-    );
+  return (
+    <section className='w-full mx-auto'>
+      <ProjectSettingForm />
+    </section>
+  );
 }
 
 export default SettingPage;
