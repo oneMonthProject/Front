76b8898098079c95a0a0abfc182b6aeb2e64087c
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-12-12 19:10:43 +0900

    feat: 프로젝트 상세 > 상단 프로젝트 info hydration 추가

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -4,6 +4,9 @@ import {redirect} from "next/navigation";
 import {useQueryString} from "@/hooks/useQueryString";
 
 function ProjectPage() {
+    // todo - accessToken 확인해서 없으면 홈페이지로 돌려보냄
+
+
     const projectId = useQueryString('projectId');
 
     // 프로젝트 상세 > 업무 탭으로 redirect
