be9777a75fc6de9374f80ed1f0e7a5859144fd89
moa1128 <moa971128@gmail.com>
2023-12-14 17:42:27 +0900

    remove: remove unused file

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -4,18 +4,22 @@ import { RecoilRoot } from 'recoil';
 import { QueryClient } from "@tanstack/query-core";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { CookiesProvider } from 'react-cookie';
 
 function Providers({ children }: { children: ReactNode }) {
     const [queryClient] = useState(() => new QueryClient());
+
+    if (process.env.NEXT_PUBLIC_API_MOCKING === 'true'){
+        import('../mocks').then(async ({setupMocks}) => {
+            setupMocks();
+        });
+    }
+    
     return (
         <RecoilRoot>
-            <CookiesProvider>
-                <QueryClientProvider client={queryClient}>
-                    {children}
-                    <ReactQueryDevtools />
-                </QueryClientProvider>
-            </CookiesProvider>
+            <QueryClientProvider client={queryClient}>
+                {children}
+                <ReactQueryDevtools />
+            </QueryClientProvider>
         </RecoilRoot>
     );
 }
