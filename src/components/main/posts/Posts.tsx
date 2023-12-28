17f47cfd2b69f9e2f95cee635755926c943e2393
moa1128 <moa971128@gmail.com>
2023-12-28 22:17:34 +0900

    chore: remove unused code

--- src/components/main/posts/Posts.tsx
+++ src/components/main/posts/Posts.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { Suspense, } from "react";
+import React, { Suspense } from "react";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { selectedPositionState } from "@/store/MainStateStore";
 import Search from "./Search";
