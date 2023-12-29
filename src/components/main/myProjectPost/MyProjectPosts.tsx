4081e24141e4ee69408874d1f9bb020784b6a3cb
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2023-12-29 21:08:42 +0900

    feat: 내 프로젝트 목록 없을 때 대체하는 Ui 추가

--- src/components/main/myProjectPost/MyProjectPosts.tsx
+++ src/components/main/myProjectPost/MyProjectPosts.tsx
@@ -16,23 +16,35 @@ function MyProjectPosts() {
 
     const projectPosts = data!.data;
 
-    // startDate기준 오름차순 정렬
-    const sortedPosts = sortByStartDate(projectPosts, 'desc');
-
     return (
         <section className='my-10'>
-            <ul
-                className="grid justify-items-center pc:grid-cols-4 tablet:grid-cols-2 mobile:grid-cols-1 mt-8 mobile:mt-2 gap-10 mobile:gap-0 mobile:bg-grey200">
-                {
-                    sortedPosts?.map(v => (
-                        <li
-                            key={v.projectId}
-                            className="flex-col w-[280px] rounded-xl border-2 shadow-lg mobile:bg-white mobile:w-full mobile:shadow-none mobile:rounded-none mobile:border-none mobile:mt-2">
-                            <ProjectCard projectPost={v}/>
-                        </li>
-                    ))}
-            </ul>
-            <CommonPagination/>
+            {
+                projectPosts.length > 0 ?
+                    (
+                        <>
+                            <ul
+                                className="grid justify-items-center pc:grid-cols-4 tablet:grid-cols-2 mobile:grid-cols-1 mt-8 mobile:mt-2 gap-10 mobile:gap-0 mobile:bg-grey200">
+                                {
+                                    sortByStartDate(projectPosts, 'desc').map(v => (
+                                        <li
+                                            key={v.projectId}
+                                            className="flex-col w-[280px] rounded-xl border-2 shadow-lg mobile:bg-white mobile:w-full mobile:shadow-none mobile:rounded-none mobile:border-none mobile:mt-2">
+                                            <ProjectCard projectPost={v}/>
+                                        </li>
+                                    ))
+                                }
+                            </ul>
+                            <CommonPagination/>
+                        </>
+                    )
+                    : (
+                        <div className='w-full bg-ground100 text-center rounded-md'>
+                            <p className='py-10 text-2xl font-medium text-grey900'>참여하고 있는 프로젝트가 없습니다.</p>
+                        </div>
+                    )
+            }
+
+
         </section>
     );
 }
