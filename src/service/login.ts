f5248a0a2ae3ff425dddddb391436d5bffff0791
moa1128 <moa971128@gmail.com>
2023-12-29 16:34:44 +0900

    feat: add credentials option at login

--- src/service/login.ts
+++ src/service/login.ts
@@ -1,22 +1,24 @@
 import { setCookie } from "cookies-next";
 
+const baseURL = process.env.NEXT_PUBLIC_BACKEND;
+
 export const login = async (email: string, password: string) => {
   const loginRequest = { email, password };
 
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND}/api/user/login/public`,
+    `${baseURL}/api/user/login/public`,
     {
       method: "POST",
       body: JSON.stringify(loginRequest),
       headers: {
         "Content-Type": "application/json",
       },
+      credentials: "include",
     }
   );
 
   const { headers } = response;
   console.log("Access", headers.get("Authorization"));
-  
   setCookie("Access", headers.get("Authorization"));
 
   return response.json();
