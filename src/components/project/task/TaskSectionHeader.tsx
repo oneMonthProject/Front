bd00e56f85a07b337e76f9f6ac223998c36df7ed
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-01-02 20:36:37 +0900

    Fix: 프로젝트 상세 > TaskSectionHeader 수정
    1. TaskAddButton 추가
    2. TaskStatusBadge 추가

--- src/components/project/task/TaskSectionHeader.tsx
+++ src/components/project/task/TaskSectionHeader.tsx
@@ -1,35 +1,25 @@
 'use client';
 import React from 'react';
-import Button from "@/components/ui/Button";
-import {FaPlus} from "@react-icons/all-files/fa/FaPlus";
-import {useRecoilState, useRecoilValue, useSetRecoilState} from "recoil";
-import {currentTaskFormState, TaskForm} from "@/store/project/task/TaskStateStore";
-import {milestoneActiveStateStore, MilestoneStatusName} from "@/store/project/task/MilestoneStateStore";
-import MilestoneStatusBadge from "@/components/ui/badge/MilestoneStatusBadge";
+import TaskAddButton from "@/components/project/task/task/TaskAddButton";
+import TaskStatusBadge from "@/components/ui/badge/TaskStatusBadge";
+import {TaskStatusName} from "@/utils/type";
 
 interface TaskSectionHeaderProps {
     content:string;
     startDate:string;
     endDate:string;
-    progressStatus: MilestoneStatusName;
+    progressStatus: TaskStatusName;
 }
-export default function TaskSectionHeader({content, startDate, endDate, progressStatus}:TaskSectionHeaderProps) {
-    const setTaskFormState = useSetRecoilState(currentTaskFormState);
+export default function TaskSectionHeader({ content, startDate, endDate, progressStatus}:TaskSectionHeaderProps) {
 
     return (
         <div className='w-full flex items-center justify-start mb-4'>
-            <Button size='md'
-                    onClickHandler={() => setTaskFormState(new TaskForm('add', null, '', false, null, null, null, '', null))}>
-                    <span className='flex items-center'>
-                        <FaPlus className='tablet:w-3 tablet:h-3 mr-2'/>
-                        업무 추가
-                    </span>
-            </Button>
+           <TaskAddButton/>
             <div className='ml-4 mr-auto flex items-center space-x-3'>
                 <h3 className='my-2 tablet:text-3xl font-medium text-greyDarkBlue'>
                     {content}
                 </h3>
-                <MilestoneStatusBadge text={progressStatus}/>
+                <TaskStatusBadge text={progressStatus}/>
                 <div className='flex items-center space-x-3 tablet:text-xl text-grey800'>
                     <span>{startDate} &#126;</span>
                     <span>{endDate}</span>
