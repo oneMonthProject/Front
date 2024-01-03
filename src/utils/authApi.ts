e455bc118908b4e06be74a92225609ffbb610632
moa1128 <moa971128@gmail.com>
2024-01-03 16:27:03 +0900

    fix: set refresh token cookie

--- src/utils/authApi.ts
+++ src/utils/authApi.ts
@@ -2,17 +2,40 @@
 import returnFetch from "return-fetch";
 import { getCookie, setCookie } from "cookies-next";
 import { cookies } from "next/headers";
+import { camelCase } from "lodash";
 
 const baseURL = process.env.NEXT_PUBLIC_BACKEND;
+
+const setRefreshCookie = (cookieStr: string) => {
+  let refreshTokenValue = "";
+  let cookieOptions = {};
+  cookieStr.split(";").map((item) => {
+    const cookieItem = item.trim().split("=");
+    if (cookieItem.includes("Refresh")) {
+      refreshTokenValue = cookieItem[1];
+    } else {
+      const optionName = camelCase(cookieItem[0]);
+      const optionValue = cookieItem[1] ?? true;
+      cookieOptions = {
+        ...cookieOptions,
+        [optionName]: optionValue,
+      };
+    }
+  });
+
+  console.log("refreshTokenValue", refreshTokenValue);
+  console.log("cookieOptions", cookieOptions);
+
+  if (refreshTokenValue) {
+    cookies().set("Refresh", refreshTokenValue, cookieOptions);
+  }
+};
 
 const authApi = returnFetch({
   baseUrl: baseURL,
   interceptors: {
     request: async (requestArgs) => {
       const accessToken = getCookie("Access", { cookies });
-      // console.log("accessToken", accessToken);
-
-      // 실제 서버와 연결했을 때 Set-Cookie 로 넘긴 cookie 가 자동으로 넘겨지는지 확인하기
       if (requestArgs[1] && accessToken) {
         requestArgs[1].headers = {
           "Content-Type": "application/json",
@@ -20,10 +43,10 @@ const authApi = returnFetch({
           Authorization: `Bearer ${accessToken}`,
         };
       }
-      
+
       return requestArgs;
     },
-    response: async (response, requestArgs, fetch) => {
+    response: async (response, requestArgs) => {
       console.log("response.status", response.status);
       if (response.status !== 401) {
         return response;
@@ -32,21 +55,27 @@ const authApi = returnFetch({
       console.log("not authorized, trying to get refresh cookie..");
 
       const userId = getCookie("user_id", { cookies });
-      // console.log("userId", userId);
-      if (userId) {
+      const refreshToken = getCookie("Refresh", { cookies });
+      if (userId && refreshToken) {
         const tokenResponse = await fetch(`${baseURL}/api/user/token-reissue`, {
           method: "POST",
+          body: JSON.stringify({ userId }),
           headers: {
             "Content-Type": "application/json",
+            Cookie: `Refresh=${refreshToken}`,
           },
           credentials: "include",
-          body: JSON.stringify({ userId }),
         });
 
-        const { status, headers } = tokenResponse;
+        if (tokenResponse.ok) {
+          const { headers } = tokenResponse;
+          const accessToken = headers.get("Authorization");
+          setCookie("Access", accessToken, { cookies });
 
-        if (status === 200) {
-          setCookie("Access", headers.get("Authorization"), { cookies });
+          const refreshToken = headers.get("Set-Cookie");
+          if (refreshToken) {
+            setRefreshCookie(refreshToken);
+          }
         } else {
           throw Error("failed to refresh cookie");
         }
@@ -54,7 +83,9 @@ const authApi = returnFetch({
         throw Error("failed to get cookies");
       }
 
-      return fetch(...requestArgs);
+      console.log("complete the reissue of cookies..!");
+
+      return await authApi(...requestArgs);
     },
   },
 });
