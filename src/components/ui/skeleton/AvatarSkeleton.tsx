f1f347e13065354105beb3c587a677d1e4fba0ce
moa1128 <moa971128@gmail.com>
2024-01-11 17:53:04 +0900

    feat: add className prop in AvatarSkeleton

--- src/components/ui/skeleton/AvatarSkeleton.tsx
+++ src/components/ui/skeleton/AvatarSkeleton.tsx
@@ -1,10 +1,10 @@
 import { makeImageSize } from '@/utils/common';
 import React from 'react';
 
-function AvatarSkeleton({size}:{size:string}) {
+function AvatarSkeleton({size, className}:{size:string, className?:string}) {
     const imageSize = makeImageSize(size);
     return (
-        <div className={`${imageSize} bg-gray-300 rounded-full animate-pulse`}>
+        <div className={`${imageSize} ${className} bg-gray-300 rounded-full animate-pulse`}>
         </div>
     );
 }
