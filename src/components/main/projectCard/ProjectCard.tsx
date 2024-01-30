f32b968590e183bf2db8e9f4c06c2435bc638cb9
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-01-30 15:49:25 +0900

    design: 프로젝트 카드 정렬 수정 & 주제 tooltip 추가

--- src/components/main/projectCard/ProjectCard.tsx
+++ src/components/main/projectCard/ProjectCard.tsx
@@ -27,38 +27,46 @@ const ProjectCard = ({projectPost}: ProjectCardProps) => {
 
     return (
         <article className='p-4'>
-            <div className="flex">
+            <div className="flex mb-[16px]">
                 <span className='sr-only'>프로젝트 이름:</span>
-                <span className="inline-block mr-3 font-bold text-base">
+                <span className="inline-block mr-3 font-bold text-xl">
                         {name}
                     </span>
                 <span className='sr-only'>프로젝트 신뢰등급:</span>
                 <TrustGradeBadge size="xs" text={trustGrade.name}/>
             </div>
-            <div className="flex items-center mt-2">
+            <div className="my-2 flex items-center text-base text-gray-600 font-medium">
+                <span className='basis-[50px] text-gray-500 font-semibold'>기간</span>
                 <span className='sr-only'>시작날짜:</span>
-                <time className="inline-block mr-2 text-sm" dateTime={`${format(new Date(startDate), 'yyyy-MM-dd')}`}>
-                        {`${format(new Date(startDate), 'yyyy-MM-dd')}`}
+                <time className="inline-block mr-1 " dateTime={`${format(new Date(startDate), 'yyyy-MM-dd')}`}>
+                    {`${format(new Date(startDate), 'yyyy-MM-dd')}`}
                 </time>
                 <span className='text-sm' aria-hidden={true}>&#126;</span>
                 <span className='sr-only'>종료날짜:</span>
-                <time className="inline-block ml-2 text-sm" dateTime={`${format(new Date(endDate), 'yyyy-MM-dd')}`}>
-                        {format(new Date(endDate), 'yyyy-MM-dd')}
+                <time className="inline-block ml-1" dateTime={`${format(new Date(endDate), 'yyyy-MM-dd')}`}>
+                    {format(new Date(endDate), 'yyyy-MM-dd')}
                 </time>
             </div>
-            <div className="text-xl font-bold mt-2">
-                <span className='sr-only'>프로젝트 주제:</span>
-                {subject}
+            <div className="relative my-3 flex items-start">
+                <span className='basis-[50px] text-base text-gray-500 font-semibold'>주제</span>
+                <div className=' group' data-tooltip-target='tooltip-subject'>
+                    <div className='max-w-[180px] truncate text-lg font-semibold'>{subject}</div>
+                    <div id="tooltip-subject" role="tooltip"
+                         className="w-fit absolute z-10 invisible group-hover:visible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm tooltip dark:bg-gray-700">
+                        {subject}
+                        <div className="tooltip-arrow" data-popper-arrow></div>
+                    </div>
+                </div>
             </div>
-            <div className='mt-5 flex items-center space-x-3'>
-                <span className='sr-only'>프로젝트 멤버:</span>
-                <ul className="flex -space-x-3 overflow-hidden">
+            <div className='my-2 flex items-center space-x-3'>
+                <span className='basis-[50px] text-gray-500 font-bold'>멤버</span>
+                <ul className="w-full flex -space-x-3 overflow-hidden">
                     {visibleMembers.map(
                         v => (
                             <li key={v.projectMemberId}>
                                 <Avatar
                                     size='xs'
-                                    alt={v.user.nickname}
+                                    alt='프로젝트 멤버 프로필 이미지'
                                     src={v.user.profileImgSrc}
                                 />
                             </li>
@@ -74,16 +82,20 @@ const ProjectCard = ({projectPost}: ProjectCardProps) => {
                     </div>
                 }
             </div>
-            <div className="flex my-5">
-                <Image src="/images/update.svg" alt="icon" width={15} height={15}/>
+            <div className="flex my-5 text-base">
+                <span className='basis-[30px] text-gray-500 font-semibold'>
+                    <Image src="/images/update.svg" alt="update date" width={20} height={20} className='mx-auto'/>
+                </span>
                 <span
-                    className="inline-block ml-1 font-bold text-grey800 text-sm">
-                        {`업데이트 : ${format(new Date(updateDate), 'yyyy-MM-dd')}`}
+                    className="ml-5 inline-block font-bold text-grey800">
+                        {format(new Date(updateDate), 'yyyy-MM-dd')}
                     </span>
             </div>
             <div className='flex justify-center mt-5 mb-3'>
-                <Link href={`/project?projectId=${projectId}`} className='mobile:py-1 tablet:py-1.5 mobile:px-3 tablet:px-3.5 mobile:text-sm tablet:text-base text-white rounded-full font-semibold shadow-sm bg-primary' aria-label={`${name} 페이지로 이동`}>
-                        프로젝트로 이동
+                <Link href={`/project?projectId=${projectId}`}
+                      className='mobile:py-1 tablet:py-1.5 mobile:px-3 tablet:px-3.5 mobile:text-sm tablet:text-base text-white rounded-full font-semibold shadow-sm bg-primary'
+                      aria-label={`${name} 페이지로 이동`}>
+                    프로젝트로 이동
                 </Link>
             </div>
         </article>
