147363b250e4a243ce7848a0ad6bd4d2ceb49508
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-02-02 18:58:13 +0900

    feat: 프로젝트 알림 생성 api 연동

--- src/service/project/notice.ts
+++ src/service/project/notice.ts
@@ -1,4 +1,5 @@
 import {request} from "@/service/project/request";
+import {NoticeCreateForm, TaskStatusCode} from "@/utils/type";
 
 /**
  * 프로젝트 알림 목록 조회
@@ -10,4 +11,17 @@ export async function getProjectNoticeList(projectId: string | bigint, pageIndex
         `/api/project/notice?projectId=${projectId}&pageIndex=${pageIndex}&itemCount=${itemCount}`
     );
 }
+
+/**
+ * 업무 알림 생성
+ * @param status
+ * @param userNickname
+ * @param noticeCreateForm
+ */
+export async function createProjectTaskNotice(noticeCreateForm:NoticeCreateForm){
+    return await request('POST', '/api/project/notice',{...noticeCreateForm});
+}
+
+
+
 
