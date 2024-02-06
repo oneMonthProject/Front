8962527a0adf50764e6e777b6151a9afb4d9fb7c
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-02-06 21:43:14 +0900

    design: 메인 > 게시글 카드 스타일 수정(간격 조절, truncate 추가)

--- src/components/main/postCard/PostCard.tsx
+++ src/components/main/postCard/PostCard.tsx
@@ -4,50 +4,118 @@ import TrustGradeBadge from "@/components/ui/badge/TrustGradeBadge";
 import PositionBadge from "@/components/ui/badge/PositionBadge";
 import TechStackImage from "@/components/ui/TechStackImage";
 import Avatar from "@/components/ui/Avatar";
-import { BsEyeFill } from "@react-icons/all-files/bs/BsEyeFill";
-import { PostCardInfo } from "@/utils/type";
+import {BsEyeFill} from "@react-icons/all-files/bs/BsEyeFill";
+import {PostCardInfo} from "@/utils/type";
+import {format} from "date-fns";
+import {FaPlusCircle} from "@react-icons/all-files/fa/FaPlusCircle";
 
-const PostCard = ({ postInfo }: { postInfo: PostCardInfo }) => {
-  const { boardId, boardTitle, project, boardPositions, boardPageView, user } = postInfo;
+const PostCard = ({postInfo}: { postInfo: PostCardInfo }) => {
+    const {boardId, boardTitle, project, boardPositions, boardPageView, user} = postInfo;
 
-  return (
-    <div className="p-3 flex-col w-[280px] rounded-xl border-2 shadow-lg mobile:bg-white mobile:w-full mobile:shadow-none mobile:rounded-none mobile:border-none mobile:mt-2">
-      <Link href={`/post?postId=${boardId}`}>
-        <div className="flex">
-          <div className="text-base mr-3 max-w-[180px] truncate">{project.name}</div>
-          <TrustGradeBadge size="xs" text={project.trustGrade.name} />
-        </div>
-        <div className="mt-2 text-xs">
-          {`${project.startDate} ~ ${project.endDate}`}
-        </div>
-        <div className="text-sm mt-2 font-bold text-grey800 truncate">
-          {`주제 | ${project.subject}`}
-        </div>
-        <div className="text-xl font-bold mt-2 truncate">{boardTitle}</div>
-        <div className="flex gap-1 mt-2 overflow-scroll">
-          {boardPositions.length > 0 && boardPositions.map((boardPosition) => {
-            const { positionId, name } = boardPosition.position;
-            return <PositionBadge key={positionId.toString()} size="xs" text={name} />
-          })}
-        </div>
-        <div className="flex gap-1 border-b-2 pb-4 mt-4 overflow-scroll">
-          {project.technologyStacks.length > 0 && project.technologyStacks.map((stack) => (
-            <TechStackImage key={stack.techStackId.toString()} stackName={stack.techStackName} width={40} height={40} />
-          ))}
-        </div>
-        <div className="flex items-center mt-2 px-1 justify-between">
-          <div className="flex">
-            <Avatar size="2xs" alt="사용자" src={user.profileImgSrc} />
-            <div className="ml-2 text-sm self-center">{user.nickname}</div>
-          </div>
-          <div className="flex">
-            <BsEyeFill className="w-4 h-4 self-center text-grey500" />
-            <div className="ml-2 text-base text-grey800">{boardPageView}</div>
-          </div>
-        </div>
-      </Link>
-    </div>
-  );
+    const {name: projectName, trustGrade, startDate, endDate, subject, technologyStacks} = project;
+
+    return (
+        <article className='p-4'>
+            <Link href={`/post?postId=${boardId}`}>
+                <div className="text-xl font-bold truncate mb-1">{boardTitle}</div>
+                <div className="mt-4 mb-2 flex items-center text-base text-gray-600 font-medium">
+                    <span className='basis-[50px] text-gray-500 font-semibold'>기간</span>
+                    <span className='sr-only'>시작날짜:</span>
+                    <time className="inline-block mr-1 " dateTime={`${format(new Date(startDate), 'yyyy-MM-dd')}`}>
+                        {`${format(new Date(startDate), 'yyyy-MM-dd')}`}
+                    </time>
+                    <span className='text-sm' aria-hidden={true}>&#126;</span>
+                    <span className='sr-only'>종료날짜:</span>
+                    <time className="inline-block ml-1" dateTime={`${format(new Date(endDate), 'yyyy-MM-dd')}`}>
+                        {format(new Date(endDate), 'yyyy-MM-dd')}
+                    </time>
+                </div>
+                <div className="flex my-2">
+                    <span className='sr-only'>프로젝트 이름:</span>
+                    <span className='basis-[50px] text-base text-gray-500 font-semibold'>팀명</span>
+                    <span className="inline-block max-w-[100px] mr-3 font-bold text-lg truncate">
+                        {projectName}
+                    </span>
+                    <span className='sr-only'>프로젝트 신뢰등급:</span>
+                    <TrustGradeBadge size="xs" text={trustGrade.name}/>
+                </div>
+                <div className="relative my-2 flex items-start">
+                    <span className='basis-[50px] text-base text-gray-500 font-semibold'>주제</span>
+                    <div className='group'>
+                        <div className='max-w-[180px] truncate text-lg font-semibold'>{subject}</div>
+                        <div id="tooltip-subject" role="tooltip"
+                             className="customTooltip group-hover:visible">
+                            {subject}
+                        </div>
+                    </div>
+                </div>
+
+                {
+                    boardPositions.length > 0 &&
+                    <div className="flex items-center gap-2 mt-5">
+                        <ul className='flex items-center basis-[180px]'>
+                            {
+                                boardPositions.slice(0, 3).map(
+                                    ({position: {positionId, name}}) =>
+                                        (
+                                            <li key={positionId.toString()}>
+                                                <PositionBadge key={positionId.toString()} size="xs" text={name}/>
+                                            </li>
+                                        )
+                                )
+                            }
+                        </ul>
+                        {
+                            boardPositions.length > 3 &&
+                            <div className='flex items-center space-x-2'>
+                                <FaPlusCircle/>
+                                <span
+                                    className='pt-1 leading-none text-greyDarkblue/80 font-semibold'>{boardPositions.length - 3}</span>
+                            </div>
+                        }
+                    </div>
+                }
+                {
+                    technologyStacks.length > 0 &&
+                    <div className="flex items-center gap-2 border-b-2 pb-4 mt-3 ">
+                        <ul className='flex items-center space-x-1 basis-[180px]'>
+                            {
+                                technologyStacks.slice(0,5).map(({techStackId, techStackName}) => (
+                                    <li key={techStackId}>
+                                        <TechStackImage
+                                            key={techStackId}
+                                            stackName={techStackName}
+                                            width={30}
+                                            height={30}/>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                        {
+                            technologyStacks.length > 5 &&
+                            <div className='flex items-center space-x-2'>
+                                <FaPlusCircle/>
+                                <span
+                                    className='pt-1 leading-none text-greyDarkblue/80 font-semibold'>
+                                    {technologyStacks.length - 5}
+                                </span>
+                            </div>
+                        }
+                    </div>
+                }
+                <div className="flex items-center pt-3 px-1 justify-between">
+                    <div className="flex">
+                        <Avatar size="2xs" alt="사용자" src={user.profileImgSrc}/>
+                        <div className="ml-2 text-sm self-center">{user.nickname}</div>
+                    </div>
+                    <div className="flex">
+                        <BsEyeFill className="w-4 h-4 self-center text-grey500"/>
+                        <div className="ml-2 text-base text-grey800">{boardPageView}</div>
+                    </div>
+                </div>
+            </Link>
+        </article>
+    );
 };
 
 export default PostCard;
--- src/components/main/posts/PostList.tsx
+++ src/components/main/posts/PostList.tsx
@@ -2,48 +2,63 @@
 
 import CommonPagination from "@/components/ui/CommonPagination"
 import PostCard from "../postCard/PostCard"
-import { useRecoilValue } from "recoil";
-import { useState } from "react";
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { PageResponseBody, PostCardInfo } from "@/utils/type";
-import { getPostList } from "@/service/post/post";
-import { postSearchValue, selectedPositionState, selectedTechStackState } from "@/store/post/PostStateStore";
+import {useRecoilValue} from "recoil";
+import React, {useState} from "react";
+import {useSuspenseQuery} from "@tanstack/react-query";
+import {PageResponseBody, PostCardInfo} from "@/utils/type";
+import {getPostList} from "@/service/post/post";
+import {postSearchValue, selectedPositionState, selectedTechStackState} from "@/store/post/PostStateStore";
 
 const PostList = () => {
-  const selectedTechStacks = useRecoilValue(selectedTechStackState);
-  const selectedPosition = useRecoilValue(selectedPositionState);
-  const searchValue = useRecoilValue(postSearchValue);
-  const [pageNumber, setPageNumber] = useState(0);
+    const selectedTechStacks = useRecoilValue(selectedTechStackState);
+    const selectedPosition = useRecoilValue(selectedPositionState);
+    const searchValue = useRecoilValue(postSearchValue);
+    const [pageNumber, setPageNumber] = useState(0);
 
-  const { data } = useSuspenseQuery<PageResponseBody<PostCardInfo[]>, Error>({
-    queryKey: ['postList', selectedTechStacks, selectedPosition, searchValue, pageNumber],
-    queryFn: () => getPostList({ techStacks: selectedTechStacks, position: selectedPosition, keyword: searchValue, page: pageNumber })
-  });
+    const {data} = useSuspenseQuery<PageResponseBody<PostCardInfo[]>, Error>({
+        queryKey: ['postList', selectedTechStacks, selectedPosition, searchValue, pageNumber],
+        queryFn: () => getPostList({
+            techStacks: selectedTechStacks,
+            position: selectedPosition,
+            keyword: searchValue,
+            page: pageNumber
+        })
+    });
 
-  const { content: infos, totalPages } = data.data;
+    const {content: infos, totalPages} = data.data;
 
-  return (
-    <div className="mt-6 mobile:mt-2">
-      {
-        infos.length > 0 ? (
-          <>
-            <div className="grid justify-items-center pc:grid-cols-4 tablet:grid-cols-2 mobile:grid-cols-1 gap-10 mobile:gap-0 mobile:bg-grey200">
-              {
-                infos.map((info) => (
-                  <PostCard key={info.boardId.toString()} postInfo={info} />
-                ))
-              }
-            </div>
-            <CommonPagination activePage={pageNumber + 1} itemsCountPerPage={8} totalItemsCount={totalPages} pageRangeDisplayed={5} onChangePageHandler={(page) => setPageNumber(page - 1)} />
-          </>
-        ) : (
-          <div className='w-full bg-ground100 text-center rounded-md mb-10 mobile:mb-4'>
-            <p className='py-10 text-2xl font-medium text-grey900'>게시글이 없습니다.</p>
-          </div>
-        )
-      }
-    </div>
-  )
+    return (
+        <section className="mt-6 mobile:mt-2">
+            {
+                infos.length > 0 ? (
+                    <ul className='grid justify-items-center pc:grid-cols-4 tablet:grid-cols-2 mobile:grid-cols-1 mt-8 mobile:mt-2 gap-10 mobile:gap-0 mobile:bg-grey200'>
+                        {
+                            infos.map((info) => (
+                                    <li
+                                        key={info.boardId.toString()}
+                                        className="flex-col w-[280px] max-h-[330px] rounded-xl border-2 shadow-lg mobile:bg-white mobile:w-full mobile:shadow-none mobile:rounded-none mobile:border-none mobile:mt-2"
+                                    >
+                                        <PostCard key={info.boardId.toString()} postInfo={info}/>
+                                    </li>
+                                )
+                            )
+
+                        }
+                        <CommonPagination
+                            activePage={pageNumber + 1}
+                            itemsCountPerPage={8}
+                            totalItemsCount={totalPages}
+                            pageRangeDisplayed={5}
+                            onChangePageHandler={(page) => setPageNumber(page - 1)}/>
+                    </ul>
+                ) : (
+                    <div className='flex items-center justify-center w-full h-[280px] bg-ground100 text-center rounded-md'>
+                        <p className='py-10 mobile:text-2xl tablet:text-3xl font-medium text-grey900'>게시글이 없습니다.</p>
+                    </div>
+                )
+            }
+        </section>
+    )
 }
 
 export default PostList;
