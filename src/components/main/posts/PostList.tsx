92ce895e56c1ce8d334cc29986727919edc16795
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-02-07 14:17:14 +0900

    fix: 메인 > 게시글 영역 pagination 중앙정렬

--- src/components/main/posts/PostList.tsx
+++ src/components/main/posts/PostList.tsx
@@ -31,28 +31,31 @@ const PostList = () => {
         <section className="mt-6 mobile:mt-2">
             {
                 infos.length > 0 ? (
-                    <ul className='grid justify-items-center pc:grid-cols-4 tablet:grid-cols-2 mobile:grid-cols-1 mt-8 mobile:mt-2 gap-10 mobile:gap-0 mobile:bg-grey200'>
-                        {
-                            infos.map((info) => (
-                                    <li
-                                        key={info.boardId.toString()}
-                                        className="flex-col w-[280px] max-h-[330px] rounded-xl border-2 shadow-lg mobile:bg-white mobile:w-full mobile:shadow-none mobile:rounded-none mobile:border-none mobile:mt-2"
-                                    >
-                                        <PostCard key={info.boardId.toString()} postInfo={info}/>
-                                    </li>
+                    <>
+                        <ul className='grid justify-items-center pc:grid-cols-4 tablet:grid-cols-2 mobile:grid-cols-1 mt-8 mobile:mt-2 gap-10 mobile:gap-0 mobile:bg-grey200'>
+                            {
+                                infos.map((info) => (
+                                        <li
+                                            key={info.boardId.toString()}
+                                            className="flex-col w-[280px] max-h-[330px] rounded-xl border-2 shadow-lg mobile:bg-white mobile:w-full mobile:shadow-none mobile:rounded-none mobile:border-none mobile:mt-2"
+                                        >
+                                            <PostCard key={info.boardId.toString()} postInfo={info}/>
+                                        </li>
+                                    )
                                 )
-                            )
 
-                        }
+                            }
+                        </ul>
                         <CommonPagination
                             activePage={pageNumber + 1}
                             itemsCountPerPage={8}
                             totalItemsCount={totalPages}
                             pageRangeDisplayed={5}
                             onChangePageHandler={(page) => setPageNumber(page - 1)}/>
-                    </ul>
+                    </>
                 ) : (
-                    <div className='flex items-center justify-center w-full h-[280px] bg-ground100 text-center rounded-md'>
+                    <div
+                        className='flex items-center justify-center w-full h-[280px] bg-ground100 text-center rounded-md'>
                         <p className='py-10 mobile:text-2xl tablet:text-3xl font-medium text-grey900'>게시글이 없습니다.</p>
                     </div>
                 )
