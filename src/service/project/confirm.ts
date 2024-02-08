60f42b1ac98e64ea0afc9b3583d507d27614a2fb
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-02-08 17:54:33 +0900

    feat: 프로젝트 상세 > 알림 컨펌 > 모집/업무/크루 컨펌 프로세스 연결

--- src/service/project/confirm.ts
+++ src/service/project/confirm.ts
@@ -22,5 +22,18 @@ export async function confirmTaskNotice(alertId: string | bigint, scoreTypeId: P
  * @param confirmResult
  */
 export async function confirmRecruitNotice(projectId: string | bigint, alertId: string | bigint, confirmResult: boolean) {
-    return await request('POST', '/api/project/confirm/recruit', {projectId, alertId, confirmResult});
+    const confirmRes = await request('POST', '/api/project/confirm/recruit', {projectId, alertId, confirmResult});
+    if (confirmRes.result === 'success') {
+        return await request('PATCH', '/api/project/notice', {alertId});
+    } else {
+        return confirmRes;
+    }
+}
+
+/**
+ * 크루 추가 알림 confirm
+ * @param alertId
+ */
+export async function confirmCrewAddNotice(alertId: string | bigint) {
+    return await request('PATCH', '/api/project/notice', {alertId});
 }
