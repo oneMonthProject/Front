8996ed1b769d4f19c20d33b93b1ff0dd0d6fc74f
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-02-09 16:15:15 +0900

    fix: 크루 상세페이지 > 업무 이력 데이터 없음 ui 추가

--- src/components/project/crews/detail/CrewTaskHistory.tsx
+++ src/components/project/crews/detail/CrewTaskHistory.tsx
@@ -26,63 +26,74 @@ function CrewTaskHistory() {
         queryFn: () => getCrewTaskHistory(projectMemberId, pageIndex, ITEM_PER_PAGE)
     });
 
-    console.log("data: ",data);
-
     const taskHistory = data.data.content;
     const totalCount = data.data.totalPages;
 
     return (
         <>
             <div className="flow-root tablet:mt-10 mobile:mt-8 mb-8 mx-2">
-                <ul role="list" className="-mb-8">
-                    {taskHistory.map((event, eventIdx) => (
-                        <li key={event.trustScoreHistoryId}>
-                            <div className="relative pb-8">
-                                {
-                                    eventIdx !== totalCount - 1
-                                        ? (
-                                            <span className="absolute left-3 top-4 -ml-px h-full w-0.5 bg-gray-200"
-                                                  aria-hidden="true"
-                                            />
-                                        )
-                                        : null
-                                }
-                                <div className="relative flex space-x-3">
-                                    <div className='flex items-center'>
-                                        {
-                                            event.point && event.point_type ?
-                                                <>
+                {
+                    taskHistory.length > 0
+                        ? (
+                            <ul role="list" className="-mb-8">
+                                {taskHistory.map((event, eventIdx) => (
+                                    <li key={event.trustScoreHistoryId}>
+                                        <div className="relative pb-8">
+                                            {
+                                                eventIdx !== taskHistory.length - 1
+                                                    ? (
+                                                        <span
+                                                            className="absolute left-3 top-4 -ml-px h-full w-0.5 bg-gray-200"
+                                                            aria-hidden="true"
+                                                        />
+                                                    )
+                                                    : null
+                                            }
+                                            <div className="relative flex space-x-3">
+                                                <div className='flex items-center'>
+                                                    {
+                                                        event.point && event.point_type ?
+                                                            <>
                                                   <span
                                                       className={`${event.point_type === 'minus' ? 'bg-danger' : 'bg-primary'} mr-4 h-6 w-6 rounded-full flex items-center justify-center ring-8 ring-white`}
                                                   >
                                                       {getIconByPointType(event.point_type)}
                                                   </span>
-                                                    <span
-                                                        className={`tablet:min-w-[40px] mobile:min-w-[30px] ${event.point_type === 'minus' ? 'text-danger' : 'text-primary'} leading-none tablet:text-xl mobile:text-lg font-semibold`}
-                                                    >
+                                                                <span
+                                                                    className={`tablet:min-w-[40px] mobile:min-w-[30px] ${event.point_type === 'minus' ? 'text-danger' : 'text-primary'} leading-none tablet:text-xl mobile:text-lg font-semibold`}
+                                                                >
                                                         {event.point}
                                                     </span>
-                                                </> : null
-                                        }
-                                    </div>
-                                    <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
-                                        <div>
-                                            <p className="tablet:text-lg mobile:text-md text-gray-500">
-                                                {event.workContent}
-                                            </p>
+                                                            </> : null
+                                                    }
+                                                </div>
+                                                <div className="flex min-w-0 flex-1 justify-between space-x-4 pt-1.5">
+                                                    <div>
+                                                        <p className="tablet:text-lg mobile:text-md text-gray-500">
+                                                            {event.workContent}
+                                                        </p>
+                                                    </div>
+                                                    <div
+                                                        className="flex items-center space-x-1 whitespace-nowrap text-right tablet:text-md mobile:text-sm text-gray-500">
+                                                        <span>{event.startDate}</span>
+                                                        <span>&#126;</span>
+                                                        <span>{event.endDate}</span>
+                                                    </div>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <div
-                                            className="flex items-center space-x-1 whitespace-nowrap text-right tablet:text-md mobile:text-sm text-gray-500">
-                                            <span>{event.startDate}</span>
-                                            <span>&#126;</span>
-                                            <span>{event.endDate}</span>
-                                        </div>
-                                    </div>
-                                </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        )
+                        :
+                        (
+                            <div
+                                className='flex flex-col items-center justify-center w-full h-[300px] text-3xl text-gray-600/90 text-center bg-gray-200/60 rounded-md'>
+                                <div className='pb-2'>데이터가 없습니다.</div>
                             </div>
-                        </li>
-                    ))}
-                </ul>
+                        )
+                }
             </div>
             <CommonPagination
                 activePage={pageIndex + 1}
