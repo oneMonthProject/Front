5cb3389ce2a36ef964e367f7e35863e8653821fc
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-02-10 22:50:05 +0900

    feat: 프로젝트 크루 상세 - 크루 탈퇴/강제탈퇴 api 연동 & 사용자 권한에 따라 탈퇴버튼 형식 달라지도록 수정

--- src/app/api/project/notice/route.ts
+++ src/app/api/project/notice/route.ts
@@ -3,25 +3,7 @@ import authApi from "@/utils/authApi";
 
 const baseURL = process.env.NEXT_PUBLIC_BACKEND;
 
-/**
- * 프로젝트 알림 생성
- * @param req
- * @constructor
- */
-export async function POST(req: NextRequest) {
-    const noticeCreateForm = await req.json();
-    const res = await authApi(
-        `${baseURL}/api/alert`,
-        {
-            method: 'POST',
-            body: JSON.stringify(noticeCreateForm)
-        }
-    );
 
-    const data = await res.json();
-
-    return NextResponse.json(data);
-}
 
 /**
  * 알림 상태 미확인 -> 확인으로 변경
