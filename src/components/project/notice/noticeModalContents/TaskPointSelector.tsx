b19b3fa6c2660c5de38f096595e0b496cca8a5c4
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-02-20 20:47:36 +0900

    fix: 만료된 업무 알림 컨텐츠에 TaskPointSelector 추가

--- src/components/project/notice/noticeModalContents/TaskPointSelector.tsx
+++ src/components/project/notice/noticeModalContents/TaskPointSelector.tsx
@@ -7,6 +7,8 @@ import {PointTypeValue, SelectItem} from "@/utils/type";
 import {useRecoilState} from "recoil";
 import {POINT_TYPE} from "@/utils/constant";
 import {TiMinus} from "@react-icons/all-files/ti/TiMinus";
+import {IoIosInformationCircleOutline} from "@react-icons/all-files/io/IoIosInformationCircleOutline";
+import {TiInfoOutline} from "@react-icons/all-files/ti/TiInfoOutline";
 
 const taskPointOptions: SelectItem[] = [
     {
@@ -21,6 +23,17 @@ const taskPointOptions: SelectItem[] = [
         name: '+ 신뢰점수',
         value: POINT_TYPE.plus
     }
+];
+
+const expiredTaskPointOptions:SelectItem[] = [
+    {
+        name: '점수 타입 선택',
+        value: null
+    },
+    {
+        name: '- 신뢰점수',
+        value: POINT_TYPE.minus
+    },
 ]
 
 function TaskPointSelector() {
@@ -32,30 +45,32 @@ function TaskPointSelector() {
         setCurrentNoticeForm(updatedNoticeForm);
     }
 
+    const selectorOptions = currentNoticeForm?.content.includes("만료") ? expiredTaskPointOptions : taskPointOptions;
+
     return (
-        <section className='mx-auto mt-12 flex flex-col items-stretch'>
+        <section className='mx-auto'>
             {
-                currentNoticeForm?.content.includes("만료") ?
-                    (
-                        <div className='w-full flex items-center justify-center mb-2 tablet:text-[1.45rem] mobile:text-xl text-grey900 font-semibold'>
-                            확인을 누르면
-                            <div className='text-gray-500 flex items-center'><TiMinus className='ml-3 mr-1' size={15}/>신뢰점수</div>
-                            가 부여됩니다.
+                currentNoticeForm?.content.includes("만료") &&
+                (
+                    <div
+                        className='w-full flex items-center justify-center mt-6 mb-2 text-xl text-grey800 font-semibold'>
+                        <TiInfoOutline size={23} className='mr-1'/>만료된 업무는
+                        <div className='text-red-700/60 flex items-center'>
+                            <TiMinus className='ml-3 mr-1' size={15}/>신뢰점수
                         </div>
-                    )
-                    :
-                    (
-                        <>
-                            <label className='max-w-[150px] tablet:text-[1.45rem] mobile:text-xl text-grey900 font-semibold mb-2'>신뢰점수
-                                부여</label>
-                            <Select
-                                items={taskPointOptions}
-                                label=''
-                                setValue={onChangeTaskPointHandler}
-                                value={taskPointOptions.find(v => v.value === (currentNoticeForm as ProjectNoticeTaskForm).scoreTypeId)!}/>
-                        </>
-                    )
+                        만 부여가능합니다.
+                    </div>
+                )
             }
+            <div className='max-w-[150px] mx-auto mt-6 flex flex-col items-stretch'>
+                <label className='max-w-[150px] tablet:text-[1.45rem] mobile:text-xl text-grey900 font-semibold mb-2'>신뢰점수
+                    부여</label>
+                <Select
+                    items={selectorOptions}
+                    label=''
+                    setValue={onChangeTaskPointHandler}
+                    value={selectorOptions.find(v => v.value === (currentNoticeForm as ProjectNoticeTaskForm).scoreTypeId)!}/>
+            </div>
         </section>
     );
 }
