19eb084924277240d585b0223c330813bd311fa0
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-04-11 00:19:57 +0900

    refactor: Select 타입 리팩토링 > Select UI 사용 컴포넌트 수정 > ProfileForm

--- src/components/user/profile/ProfileForm.tsx
+++ src/components/user/profile/ProfileForm.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { ChangeEvent, useRef, useState } from "react";
+import React, {ChangeEvent, useRef, useState} from "react";
 import MultiSelect from "@/components/ui/MultiSelect";
 import Select from "@/components/ui/Select";
 import Avatar from "@/components/ui/Avatar";
@@ -8,172 +8,195 @@ import Input from "@/components/ui/form/Input";
 import NicknameField from "@/components/ui/form/NickNameField";
 import TextArea from "@/components/ui/form/TextArea";
 import FormButton from "@/components/ui/form/FormButton";
-import { SelectItem } from "@/utils/type";
-import { getPositionSelectItem, getPositionSelectItems, getSelectItemValue, getTechStackSelectItems, isValidNickname } from "@/utils/common";
-import { useProfileInfo } from "@/hooks/useProfileInfo";
-import { deleteProfileImage as deleteProfileImageAPI, updateUser as updateUserAPI, updateUserInfo } from "@/service/user/user";
-import { useSetRecoilState } from "recoil";
-import { usePositionList } from "@/hooks/usePositionList";
-import { useTechStackList } from "@/hooks/useTechStackList";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { isEqual } from "lodash";
-import { snackbarState } from "@/store/CommonStateStore";
+import {SelectItem} from "@/utils/type";
+import {
+    bigIntToString,
+    getPositionSelectItems,
+    getTechStackSelectItems,
+    isValidNickname,
+    numStrToBigInt
+} from "@/utils/common";
+import {useProfileInfo} from "@/hooks/useProfileInfo";
+import {
+    deleteProfileImage as deleteProfileImageAPI,
+    updateUser as updateUserAPI,
+    updateUserInfo
+} from "@/service/user/user";
+import {useSetRecoilState} from "recoil";
+import {usePositionList} from "@/hooks/usePositionList";
+import {useTechStackList} from "@/hooks/useTechStackList";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {isEqual} from "lodash";
+import {snackbarState} from "@/store/CommonStateStore";
 
 function ProfileForm() {
-  const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-  const profileData = useProfileInfo();
-  const positions = usePositionList();
-  const techStacks = useTechStackList();
+    const {
+        position: {positionId: value, positionName: name},
+        profileImgSrc,
+        nickname: initNickname,
+        techStacks: initTechStack,
+        intro: initIntroduction,
+        email
+    } = useProfileInfo();
 
-  const [imageSrc, setImageSrc] = useState<string | null>(profileData?.profileImgSrc || null);
-  const [nickname, setNickname] = useState(profileData.nickname);
-  const [position, setPosition] = useState<SelectItem | null>(getPositionSelectItem(profileData.position));
-  const [techStack, setTechStack] = useState<SelectItem[]>(getTechStackSelectItems(profileData.techStacks));
-  const [selfIntroduction, setSelfIntroduction] = useState(profileData?.intro ?? "");
+    const positions = usePositionList();
+    const techStacks = useTechStackList();
 
-  const fileRef = useRef<HTMLInputElement>(null);
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
-  const [isCheckedNickname, setIsCheckedNickname] = useState(true);
-  const setSnackbar = useSetRecoilState(snackbarState);
+    const [imageSrc, setImageSrc] = useState<string | null>(profileImgSrc || null);
+    const [nickname, setNickname] = useState(initNickname);
+    const [position, setPosition] = useState<SelectItem<string, string>>(() => {
+        return {name, value: bigIntToString(value)}
+    });
+    const [techStack, setTechStack] = useState<SelectItem<string, string>[]>(getTechStackSelectItems(initTechStack));
+    const [selfIntroduction, setSelfIntroduction] = useState(initIntroduction);
 
-  const { mutate: updateUser } = useMutation({
-    mutationFn: (updateData: updateUserInfo) => updateUserAPI(updateData, selectedImage),
-    onSuccess: (data) => {
-      const { message, result } = data;
-      if (isEqual(result, "success")) {
-        setSnackbar({ show: true, type: "SUCCESS", content: message });
-        queryClient.invalidateQueries({ queryKey: ['profileInfo'] });
-        queryClient.invalidateQueries({ queryKey: ['simpleUserInfo'] });
-      } else {
-        setSnackbar({ show: true, type: "ERROR", content: message });
-      }
-    },
-    onError: (err) => {
-      console.log("err", err);
-    }
-  });
-  const { mutate: deleteProfileImage } = useMutation({
-    mutationFn: deleteProfileImageAPI,
-    onSuccess: (data) => {
-      const { result } = data;
-      if (isEqual(result, "success")) {
-        updateUserInfo();
-      }
-    }
-  });
+    const fileRef = useRef<HTMLInputElement>(null);
+    const [selectedImage, setSelectedImage] = useState<File | null>(null);
+    const [isCheckedNickname, setIsCheckedNickname] = useState(true);
+    const setSnackbar = useSetRecoilState(snackbarState);
 
-  const isValid = () => {
-    if (nickname === "") {
-      setSnackbar({ show: true, type: "ERROR", content: "닉네임을 입력해주세요." });
-      return false;
-    }
+    const {mutate: updateUser} = useMutation({
+        mutationFn: (updateData: updateUserInfo) => updateUserAPI(updateData, selectedImage),
+        onSuccess: (data) => {
+            const {message, result} = data;
+            if (isEqual(result, "success")) {
+                setSnackbar({show: true, type: "SUCCESS", content: message});
+                queryClient.invalidateQueries({queryKey: ['profileInfo']});
+                queryClient.invalidateQueries({queryKey: ['simpleUserInfo']});
+            } else {
+                setSnackbar({show: true, type: "ERROR", content: message});
+            }
+        },
+        onError: (err) => {
+            console.log("err", err);
+        }
+    });
+    const {mutate: deleteProfileImage} = useMutation({
+        mutationFn: deleteProfileImageAPI,
+        onSuccess: (data) => {
+            const {result} = data;
+            if (isEqual(result, "success")) {
+                updateUserInfo();
+            }
+        }
+    });
 
-    // 닉네임 형식이 맞지 않는 경우
-    if (!isValidNickname(nickname)) {
-      setSnackbar({ show: true, type: "ERROR", content: "닉네임은 영어 숫자 포함 6~10 자리만 가능합니다." });
-      return false;
-    }
+    const isValid = () => {
+        if (nickname === "") {
+            setSnackbar({show: true, type: "ERROR", content: "닉네임을 입력해주세요."});
+            return false;
+        }
 
-    // 닉네임 중복 확인 하지 않았을 경우
-    if (!isCheckedNickname) {
-      setSnackbar({ show: true, type: "ERROR", content: "닉네임 중복확인을 해주세요." });
-      return false;
-    }
+        // 닉네임 형식이 맞지 않는 경우
+        if (!isValidNickname(nickname)) {
+            setSnackbar({show: true, type: "ERROR", content: "닉네임은 영어 숫자 포함 6~10 자리만 가능합니다."});
+            return false;
+        }
 
-    if (!position) {
-      setSnackbar({ show: true, type: "ERROR", content: "직무를 선택해주세요." });
-      return false;
-    }
+        // 닉네임 중복 확인 하지 않았을 경우
+        if (!isCheckedNickname) {
+            setSnackbar({show: true, type: "ERROR", content: "닉네임 중복확인을 해주세요."});
+            return false;
+        }
 
-    if (techStack.length === 0) {
-      setSnackbar({ show: true, type: "ERROR", content: "관심 스택을 선택해주세요." });
-      return false;
-    }
+        if (!position) {
+            setSnackbar({show: true, type: "ERROR", content: "직무를 선택해주세요."});
+            return false;
+        }
 
-    return true;
-  }
+        if (techStack.length === 0) {
+            setSnackbar({show: true, type: "ERROR", content: "관심 스택을 선택해주세요."});
+            return false;
+        }
 
-  const updateUserInfo = () => {
-    if (position) {
-      const positionId = getSelectItemValue(position);
-      const techStackIds = techStack.map(stack => getSelectItemValue(stack));
-      const updateData = { nickname, positionId, techStackIds, intro: selfIntroduction } as updateUserInfo;
-
-      updateUser(updateData);
-    }
-  }
-
-  const saveProfile = () => {
-    if (!isValid()) {
-      return;
+        return true;
     }
 
-    if (profileData?.profileImgSrc !== null && imageSrc === null) {
-      // 기존 프로필 이미지 삭제 시, s3 에서 삭제 후 사용자 정보 업데이트
-      deleteProfileImage();
-    } else {
-      updateUserInfo();
+    const updateUserInfo = () => {
+        if (position) {
+            const positionId = numStrToBigInt(position.value);
+            const techStackIds = techStack.map(stack => numStrToBigInt(stack.value));
+            const updateData = {nickname, positionId, techStackIds, intro: selfIntroduction} as updateUserInfo;
+
+            updateUser(updateData);
+        }
     }
-  }
 
-  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
+    const saveProfile = () => {
+        if (!isValid()) {
+            return;
+        }
 
-    if (files && files.length > 0) {
-      const file = files[0];
-
-      setSelectedImage(file);
-      setImageSrc(URL.createObjectURL(file));
+        if (profileImgSrc !== null && imageSrc === null) {
+            // 기존 프로필 이미지 삭제 시, s3 에서 삭제 후 사용자 정보 업데이트
+            deleteProfileImage();
+        } else {
+            updateUserInfo();
+        }
     }
-  };
 
-  const handleFileButtonClick = () => {
-    if (fileRef.current) {
-      fileRef.current.click();
+    const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+
+        if (files && files.length > 0) {
+            const file = files[0];
+
+            setSelectedImage(file);
+            setImageSrc(URL.createObjectURL(file));
+        }
+    };
+
+    const handleFileButtonClick = () => {
+        if (fileRef.current) {
+            fileRef.current.click();
+        }
     }
-  }
 
-  const deleteImage = () => {
-    setImageSrc(null);
-    setSelectedImage(null);
+    const deleteImage = () => {
+        setImageSrc(null);
+        setSelectedImage(null);
 
-    if (fileRef.current) {
-      fileRef.current.value = '';
+        if (fileRef.current) {
+            fileRef.current.value = '';
+        }
     }
-  }
 
-  const onChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newName = e.target.value;
+    const onChangeNickname = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newName = e.target.value;
 
-    setNickname(newName);
-    if (isEqual(newName, profileData.nickname)) {
-      setIsCheckedNickname(true);
-    } else {
-      setIsCheckedNickname(false);
+        setNickname(newName);
+        if (isEqual(newName, nickname)) {
+            setIsCheckedNickname(true);
+        } else {
+            setIsCheckedNickname(false);
+        }
     }
-  }
 
-  return (
-    <div className="w-[380px] mobile:w-[300px] space-y-5 mobile:space-y-3">
-      <div className="w-full h-fit text-center">
-        <Avatar size="lg" src={imageSrc} alt="빈 프로필" />
-      </div>
-      <div className="text-center space-x-1">
-        <input ref={fileRef} type="file" accept='.png, .jpg, .jpeg, .gif' hidden onChange={handleImageChange} />
-        <Button size="md" theme="primary-hollow" onClickHandler={handleFileButtonClick}>{imageSrc === null ? "이미지 변경" : "변경"}</Button>
-        <Button size="md" theme="primary" onClickHandler={deleteImage} hidden={imageSrc === null}>삭제</Button>
-      </div>
-      <Input id="email" label="이메일" required disabled defaultValue={profileData.email} />
-      <NicknameField value={nickname} defaultValue={profileData.nickname} onChange={onChangeNickname}
-        placeholder="영문, 숫자 포함 6자 이상" setCheck={setIsCheckedNickname} required />
-      <Select value={position} setValue={setPosition} items={getPositionSelectItems(positions)} label="직무" placeholder="직무를 선택해주세요." required />
-      <MultiSelect values={techStack} setValues={setTechStack} items={getTechStackSelectItems(techStacks)} label="관심 스택" placeholder="관심 스택을 선택해주세요." required />
-      <TextArea id="information" label="자기소개" placeholder="텍스트를 입력해주세요." rows={3} cols={25}
-        value={selfIntroduction} onChange={(e) => setSelfIntroduction(e.target.value)} />
-      <FormButton onClick={saveProfile}>저장</FormButton>
-    </div>
-  )
+    return (
+        <div className="w-[380px] mobile:w-[300px] space-y-5 mobile:space-y-3">
+            <div className="w-full h-fit text-center">
+                <Avatar size="lg" src={imageSrc} alt="빈 프로필"/>
+            </div>
+            <div className="text-center space-x-1">
+                <input ref={fileRef} type="file" accept='.png, .jpg, .jpeg, .gif' hidden onChange={handleImageChange}/>
+                <Button size="md" theme="primary-hollow"
+                        onClickHandler={handleFileButtonClick}>{imageSrc === null ? "이미지 변경" : "변경"}</Button>
+                <Button size="md" theme="primary" onClickHandler={deleteImage} hidden={imageSrc === null}>삭제</Button>
+            </div>
+            <Input id="email" label="이메일" required disabled defaultValue={email}/>
+            <NicknameField value={nickname} defaultValue={initNickname} onChange={onChangeNickname}
+                           placeholder="영문, 숫자 포함 6자 이상" setCheck={setIsCheckedNickname} required/>
+            <Select value={position} setValue={setPosition} items={getPositionSelectItems(positions)} label="직무"
+                    placeholder="직무를 선택해주세요." required/>
+            <MultiSelect values={techStack} setValues={setTechStack} items={getTechStackSelectItems(techStacks)}
+                         label="관심 스택" placeholder="관심 스택을 선택해주세요." required/>
+            <TextArea id="information" label="자기소개" placeholder="텍스트를 입력해주세요." rows={3} cols={25}
+                      value={selfIntroduction} onChange={(e) => setSelfIntroduction(e.target.value)}/>
+            <FormButton onClick={saveProfile}>저장</FormButton>
+        </div>
+    )
 }
 
 export default ProfileForm;
