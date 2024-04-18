a762012d25029ab73ea9a048a85811039aa1edf9
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-04-18 20:33:52 +0900

    fix: useGradeListByUser hook 에러 수정

--- src/hooks/useGradeListByUser.tsx
+++ src/hooks/useGradeListByUser.tsx
@@ -1,19 +1,18 @@
 'use client';
 
 import {useQuery} from "@tanstack/react-query";
-import {ResponseBody, SelectItem, TrustGradeItem} from "@/utils/type";
+import {ResponseBody, TrustGradeItem} from "@/utils/type";
 import {getTrustGradeListByUser} from "@/service/user/user";
-import {TrustGradeIdType} from "@/app/project/@setting/_utils/type";
 
-export default function useGradeListByUser(trustGrade:SelectItem<string, TrustGradeIdType>){
+export default function useGradeListByUser(){
     const {
         data,
         isFetching,
-        status
+        isError
     } = useQuery<Promise<ResponseBody<TrustGradeItem[]>>, Error, ResponseBody<TrustGradeItem[]>>({
-        queryKey: ['trustGradeListByUser', trustGrade],
+        queryKey: ['trustGradeListByUser'],
         queryFn: () => getTrustGradeListByUser()
     });
 
-    return {gradeList:data?.data || [], isFetching};
+    return {gradeList:data?.data || [], isFetching, isError};
 }
