e3bd3f55d321394b56b3e54c42ae58ddd3e266e3
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-05-29 18:23:20 +0900

    fix: 프로젝트 상세>알림> 루 강제탈퇴 신청 후 알림목록 쿼리 refresh

--- src/components/project/crews/detail/CrewOutButton.tsx
+++ src/components/project/crews/detail/CrewOutButton.tsx
@@ -19,6 +19,7 @@ function CrewOutButton({projectMemberInfo}: { projectMemberInfo: ProjectMemberPr
         if (confirm("프로젝트 탈퇴 신청을 하시겠습니까?")) {
             const res = await createProjectCrewOutNotice(projectMemberId);
             if (res.result === 'success') {
+                await queryClient.invalidateQueries({queryKey:['noticeList']});
                 setSnackBar({show: true, type: 'SUCCESS', content: '프로젝트 탈퇴 신청 알림이 발송되었습니다.'});
             } else {
                 setSnackBar({show: true, type: 'ERROR', content: res.message});
@@ -30,6 +31,7 @@ function CrewOutButton({projectMemberInfo}: { projectMemberInfo: ProjectMemberPr
         if (confirm("크루 강제탈퇴를 신청하시겠습니까?")) {
             const res = await createProjectCrewForceOutNotice(projectMemberId);
             if (res.result === 'success') {
+                await queryClient.invalidateQueries({queryKey:['noticeList']});
                 setSnackBar({show: true, type: 'SUCCESS', content: '크루 강제 탈퇴 신청을 완료했습니다.'});
             } else {
                 setSnackBar({show: true, type: 'ERROR', content: res.message});
