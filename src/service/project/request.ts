b7056033e106ec2fb9694722d053cc5079857233
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-06-05 23:11:03 +0900

    fix:배포환경테스트

--- src/service/project/request.ts
+++ src/service/project/request.ts
@@ -24,7 +24,7 @@ export async function authRequest(method: HTTP_METHOD, url: string, data?: Recor
     };
     if (method !== 'GET' && data) requestInit.body = JSONReplaceBigInt(data);
     console.log("env:: ", process.env.NODE_ENV);
-    console.log("production:: ", process.env.NODE_ENV);
+    console.log("production:: ", process.env.NEXT_PUBLIC_PRODUCTION);
 
     const res = await fetch(`${publicURL}${url}`, requestInit);
     return res.json();
