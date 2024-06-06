9218813b2090cb023b8b0836b2c12b9ff9b72958
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-06-07 02:05:08 +0900

    fix: CORS

--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -28,23 +28,3 @@ export async function POST(req: NextRequest) {
   const data = await res.json();
   return NextResponse.json(data);
 }
-
-export async function OPTIONS(request: Request) {
-  const allowedOrigins = (process.env?.ALLOWED_ORIGIN || "").split(",");
-  const origin = request.headers.get('origin')
-  console.log("OPTIONS req Origin:::: ", origin);
-
-  const exposedHeaders = (process.env?.EXPOSED_HEADERS || "").split(",");
-  const maxAge = process.env?.MAX_AGE && parseInt(process.env?.MAX_AGE) || undefined;
-
-  return new NextResponse(null, {
-    status: 204,
-    headers: {
-      'Access-Control-Allow-Origin': origin || "*",
-      'Access-Control-Allow-Methods': 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-      'Access-Control-Allow-Headers': '*',
-      "Access-Control-Expose-Headers": exposedHeaders.join(","),
-      "Access-Control-Max-Age": maxAge?.toString() ?? ""
-    }
-  })
-}
--- src/service/project/request.ts
+++ src/service/project/request.ts
@@ -4,9 +4,9 @@ import {JSONReplaceBigInt} from "@/utils/common";
 export const publicURL = process.env.NEXT_PUBLIC_URL;
 export const headers = {
     'Content-Type': 'application/json',
-    'Origin':'http://3.35.111.141',
-    'Access-Control-Request-Method':'POST',
-    'Access-Control-Request-Headers':'Content-Type'
+    'Origin': 'http://3.35.111.141',
+    'Access-Control-Request-Method': 'POST',
+    'Access-Control-Request-Headers': 'Content-Type'
 };
 
 
@@ -26,26 +26,7 @@ export async function authRequest(method: HTTP_METHOD, url: string, data?: Recor
         headers, method, credentials: 'include'
     };
     if (method !== 'GET' && data) requestInit.body = JSONReplaceBigInt(data);
-    console.log("env:: ", process.env.NODE_ENV);
-    console.log("publicURL:: ", process.env.NEXT_PUBLIC_URL);
-    console.log("publicURL2:: ", publicURL);
 
-    let res:Response;
-    try{
-        res = await fetch(`${process.env.NEXT_PUBLIC_URL}${url}`, requestInit);
-        const headers = res.headers;
-        console.log("res::: ", headers.get("Access-Control-Allow-Origin"));
-        console.log("res::: ", headers.get("Access-Control-Allow-Methods"));
-        console.log("res::: ", headers.get("Access-Control-Allow-Headers"));
-        console.log("res.status: ", res.status);
-        console.log("res.statusText: ", res.statusText);
-        console.log("res.type: ", res.type);
-        console.log("res.redirected: ", res.redirected);
-        return res.json();
-    }catch(e:unknown){
-        console.log("ERROR::::: ", (e as Error).message);
-        console.log("ERROR STACk::::: ", (e as Error).stack);
-        console.log("ERROR CAUSE::::: ", (e as Error).cause);
-    }
-
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}${url}`, requestInit);
+    return res.json();
 }
