582a4b15b47362555d8e6b9986f99dded2135abd
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-06-07 00:34:39 +0900

    fix: login route handler에 OPTIONS추가

--- src/service/project/request.ts
+++ src/service/project/request.ts
@@ -3,7 +3,10 @@ import {JSONReplaceBigInt} from "@/utils/common";
 
 export const publicURL = process.env.NEXT_PUBLIC_URL;
 export const headers = {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
+    'Origin':'http://3.35.111.141',
+    'Access-Control-Request-Method':'POST',
+    'Access-Control-Request-Headers':'Content-Type'
 };
 
 
@@ -27,8 +30,9 @@ export async function authRequest(method: HTTP_METHOD, url: string, data?: Recor
     console.log("publicURL:: ", process.env.NEXT_PUBLIC_URL);
     console.log("publicURL2:: ", publicURL);
 
+    let res:Response;
     try{
-        const res = await fetch(`${process.env.NEXT_PUBLIC_URL}${url}`, requestInit);
+        res = await fetch(`${process.env.NEXT_PUBLIC_URL}${url}`, requestInit);
         const headers = res.headers;
         console.log("res::: ", headers.get("Access-Control-Allow-Origin"));
         console.log("res::: ", headers.get("Access-Control-Allow-Methods"));
