dfd6a0ab54db015a56382ea809d734a681ff3626
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-06-19 01:05:03 +0900

    fix: 인증포함요청/리디렉션 로직 수정
     authApiResponse에서 인증만료(401)응답 그대로 내보내도록, requestWithAuth에서 로그인 페이지로 직접 redirect하도록 수정

--- src/app/api/_utils/authApiResponse.ts
+++ src/app/api/_utils/authApiResponse.ts
@@ -1,14 +1,11 @@
 import {NextRequest, NextResponse} from "next/server";
+import {redirect} from "next/navigation";
 
 export async function authApiResponse(req: NextRequest, res: Response) {
     if (res.ok) {
         const data = await res.json();
         return NextResponse.json(data);
     } else {
-        if(res.status === 401){
-            return NextResponse.redirect(`${process.env.NEXT_PUBLIC_URL}/login`);
-        }else{
-            return res;
-        }
+        return res;
     }
 }
--- src/service/project/request.ts
+++ src/service/project/request.ts
@@ -17,17 +17,17 @@ export async function request(method: HTTP_METHOD, url: string, data?: Record<st
 
 export async function requestWithAuth(method: HTTP_METHOD, url: string, data?: Record<string, unknown>) {
     const requestInit: RequestInit = {
-        headers, method
+        headers, method, cache: 'no-cache'
     }
     if (method !== 'GET' && data) requestInit.body = JSONReplaceBigInt(data);
 
     const res = await fetch(`${publicURL}${url}`, requestInit);
 
-    if (res.status === 307) {
+    if (res.status === 401) {
         if (confirm("로그인이 만료되었습니다.")) {
-            window.location.href = res.headers.get("Location") as string;
+            window.location.replace(`${process.env.NEXT_PUBLIC_URL}/login`);
+            return;
         }
-        return;
     } else {
         return res.json();
     }
