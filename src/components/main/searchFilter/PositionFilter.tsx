632511870452089a2146bcf7c3e07fdf77e39999
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-07-01 17:22:59 +0900

    fix: 메인게시판 - 포지션필터 스켈레톤 수정

--- src/components/main/searchFilter/PositionFilter.tsx
+++ src/components/main/searchFilter/PositionFilter.tsx
@@ -13,12 +13,14 @@ function PositionFilter() {
     const {data: positions, isFetching} = usePositionList();
 
     if (isFetching) return (
-        <div
-            className="px-4 flex justify-between w-[150px] h-[40px] mobile:w-[130px] mobile:h-[35px] items-center border-2 rounded-3xl cursor-pointer bg-gray-300 animate-pulse">
-            <div className="text-base text-grey800 mobile:text-sm">
-                {"포지션"}
+        <div className='relative z-10 self-center'>
+            <div
+                className="px-4 flex justify-between w-[150px] h-[40px] mobile:w-[130px] mobile:h-[35px] items-center border-2 rounded-3xl cursor-pointer bg-gray-300 animate-pulse">
+                <div className="text-base text-grey800 mobile:text-sm">
+                    {"포지션"}
+                </div>
+                <BsChevronDown className="w-4 h-4 text-grey800"/>
             </div>
-            <BsChevronDown className="w-4 h-4 text-grey800"/>
         </div>
     );
 
