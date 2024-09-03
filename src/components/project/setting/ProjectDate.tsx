24acc9a6812d4fdd36a27f6bb6693eab8d2bb8de
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-09-03 18:54:28 +0900

    fix: 프로젝트상세 > 설정페이지 세분화 - form의 시작날짜 종료날짜 앞서지 못하도록 수정

--- src/components/project/setting/ProjectDate.tsx
+++ src/components/project/setting/ProjectDate.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import CalendarInput from "@/components/ui/form/CalendarInput";
 import {useRecoilState} from "recoil";
 import {
@@ -6,7 +6,10 @@ import {
     ProjectSettingInfoUpdField
 } from "@/store/project/setting/ProjectSettingFormStateStore";
 import {ProjectSettingInfoData} from "@/service/project/setting/info";
+import {addDays, format} from "date-fns";
 
+type ProjectSettingInfoStartDate = ProjectSettingInfoUpdField<'startDate'>;
+type ProjectSettingInfoEndDate = ProjectSettingInfoUpdField<'endDate'>;
 
 function ProjectDate({
                          initStartDate,
@@ -18,11 +21,25 @@ function ProjectDate({
     const [startDate, setStartDate] = useRecoilState(projectSettingInfoSelector('startDate'));
     const [endDate, setEndDate] = useRecoilState(projectSettingInfoSelector('endDate'));
 
-    const startDateValue = startDate ? startDate as ProjectSettingInfoUpdField<'startDate'> : initStartDate;
-    const endDateValue = endDate ? endDate as ProjectSettingInfoUpdField<'endDate'> : initEndDate;
+    const startDateValue = startDate ? startDate as ProjectSettingInfoStartDate : initStartDate;
+    const endDateValue = endDate ? endDate as ProjectSettingInfoEndDate : initEndDate;
+
+    const [endMinDate, setEndMinDate] = useState<Date | null>(() => addDays(new Date(startDateValue), 1));
+
+    // 시작날짜보다 종료날짜 앞서지 못하도록
+    useEffect(() => {
+        const endDateValueNum = parseInt(endDateValue.replaceAll("-", ""));
+        const startDateValueNum = parseInt(startDateValue.replaceAll('-', ""));
+        const initEndMinDate = addDays(new Date(startDateValue), 1);
+        if (endDateValueNum <= startDateValueNum) {
+            setEndDate(format(initEndMinDate, 'yyyy-MM-dd'));
+        }
+        setEndMinDate(initEndMinDate);
+
+    }, [startDateValue, endDateValue, setEndDate, setEndMinDate]);
 
     return (
-        <div className="w-[380px] mobile:w-full space-y-5 mobile:space-y-3 mobile:mx-auto">
+        <div className="w-[380px] tablet:w-full space-y-10 mobile:mx-auto">
             <CalendarInput
                 id="startDate"
                 label="시작 날짜"
@@ -36,6 +53,7 @@ function ProjectDate({
                 placeholder="날짜를 선택해주세요."
                 date={endDateValue}
                 setDate={(endDate) => setEndDate(endDate)}
+                minDate={endMinDate}
             />
         </div>
     );
