b726a16bd220d6d7462894f55d290c01e7cbd824
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-09-03 16:15:49 +0900

    feat: 프로젝트 상세> 설정 페이지 세분화 - 게시글정보 store, 필드 selector 추가

--- src/store/project/setting/ProjectSettingFormStateStore.tsx
+++ src/store/project/setting/ProjectSettingFormStateStore.tsx
@@ -1,58 +1,38 @@
-import {ProjectSettingForm, ProjectSettingFormKey} from "@/app/project/@setting/_utils/type";
-import {atom, DefaultValue, selector, selectorFamily} from "recoil";
-import {TRUST_GRADE as TG} from "@/app/project/@setting/_utils/constant";
-import {projectInfoState} from "@/store/project/ProjectInfoStateStore";
-import {ResponseBody} from "@/utils/type";
+import {atom, DefaultValue, selectorFamily} from "recoil";
+import {ProjectSettingInfoUpdReqData} from "@/service/project/setting/info";
 
 
-export const projectSettingFormState = atom<ResponseBody<ProjectSettingForm | null>>({
-    key: 'projectSettingFormState',
-    default: selector<ResponseBody<ProjectSettingForm | null>>({
-        key: 'projectSettingFormSelector',
-        get: ({get}) => {
-            const res = get(projectInfoState(null));
-            if (res.result !== "success" || res.data === null) return res as ResponseBody<null>;
-
-            const {
-                projectId,
-                name: projectName,
-                subject,
-                startDate,
-                endDate
-            } = res.data;
-
-            const projectSettingForm: ProjectSettingForm = {
-                projectId,
-                projectName,
-                subject,
-                startDate,
-                endDate
-            }
-
-            return {data: projectSettingForm, result: res.result, message: res.message};
-        }
-    })
+export const projectSettingInfoStateStore = atom<ProjectSettingInfoUpdReqData>({
+    key: 'projectSettingInfoStateStore',
+    default: {
+        projectId: 0n,
+        authMap: {
+            milestoneAuth: false,
+            workAuth: false,
+            voteAuth: false
+        },
+        projectName: '',
+        projectSubject: '',
+        startDate: '',
+        endDate: '',
+        technologyIds: []
+    }
 });
 
-
-export type ProjectSettingField<T> = ProjectSettingForm[Extract<ProjectSettingFormKey, T>];
-export const projectInfoFieldSelector = selectorFamily({
-    key: 'projectInfoField',
-    get: (param: ProjectSettingFormKey) => ({get}) => {
-        const res = get(projectSettingFormState);
-        if (res.result !== "success" || res.data === null) return null;
-
-        const projectSettingForm = res.data;
-        return projectSettingForm[param] as ProjectSettingField<typeof param>;
+export type ProjectSettingInfoUpdKey = keyof ProjectSettingInfoUpdReqData;
+export type ProjectSettingInfoUpdField<T> = ProjectSettingInfoUpdReqData[Extract<ProjectSettingInfoUpdKey, T>];
+export const projectSettingInfoSelector = selectorFamily({
+    key: 'projectSettingInfoSelector',
+    get: (param: ProjectSettingInfoUpdKey) => ({get}) => {
+        const res = get(projectSettingInfoStateStore);
+        return res[param] as ProjectSettingInfoUpdField<typeof param>;
     },
-    set: (param: ProjectSettingFormKey) => ({get, set}, newValue) => {
+    set: (param: ProjectSettingInfoUpdKey) => ({get, set}, newValue) => {
         if (newValue instanceof DefaultValue) return;
 
-        const projectSettingForm = get(projectSettingFormState)!;
-        const data = projectSettingForm.data!;
-        const updatedData = {...data, [param]: newValue};
-        const result = {...projectSettingForm, data:updatedData};
-        set(projectSettingFormState, result);
+        const projectSettingInfoUpdReq = get(projectSettingInfoStateStore);
+        const updated = {...projectSettingInfoUpdReq, [param]: newValue};
+        set(projectSettingInfoStateStore, updated);
     }
 })
 
