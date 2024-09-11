9ee0f9ae0e714a6d9d493fe1e136ef1e27c829ac
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-09-11 17:13:42 +0900

    style: 안 쓰는 코드 정리

--- src/app/project/@setting/_utils/type.ts
+++ src/app/project/@setting/_utils/type.ts
@@ -5,17 +5,3 @@ export type TrustGradeSelectType = ArrayValue<typeof TRUST_GRADE.values>;
 export type TrustGradeNameType = TrustGradeSelectType['name'];
 export type TrustGradeValueType = TrustGradeSelectType['value'];
 
-export type ProjectSettingForm = {
-    projectId: bigint;
-    projectName: string;
-    subject: string;
-    startDate: string;
-    endDate: string;
-}
-
-export type ProjectSettingFormKey = keyof ProjectSettingForm;
-export type ProjectSettingFormValue = ProjectSettingForm[ProjectSettingFormKey];
-
-export type ProjectInfoUpdateReq = ProjectSettingForm & {
-    authMap: ProjectAuthMap;
-}
