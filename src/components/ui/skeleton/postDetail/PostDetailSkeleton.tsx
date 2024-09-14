2700ce22ecfae7803092f1afef355869ac19632b
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-09-14 21:53:03 +0900

    design: 메인 > 게시글 상세페이지 Skeleton layout 틀어짐 수정

--- src/components/ui/skeleton/postDetail/PostDetailSkeleton.tsx
+++ src/components/ui/skeleton/postDetail/PostDetailSkeleton.tsx
@@ -1,15 +1,16 @@
 import AvatarSkeleton from "@/components/ui/skeleton/AvatarSkeleton";
 import Skeleton from "@/components/ui/skeleton/Skeleton";
+import React from "react";
 
 const PostDetailSkeleton = () => {
   return (
-    <div className="p-5 mobile:p-1 m-auto">
-      <div className="flex-col">
-        <Skeleton sizeClassName="w-40 h-10 mobile:h-8 mt-5" />
-        <div className="flex gap-3 items-center mt-8 mobile:mt-3 border-b-2 pb-8 mobile:pb-3">
+    <div className="p-5 mobile:p-1">
+      <div className="h-[130px] mobile:h-[100px] flex flex-col justify-center mt-5 mb-5 mobile:mt-0 space-y-6 mobile:space-y-3 border-b-2">
+        <Skeleton sizeClassName="w-[300px] h-10 mobile:h-8 mt-5" />
+        <div className="flex gap-3 items-center">
           <div className="flex items-center gap-2">
             <AvatarSkeleton size="md" className='relative inline-block h-8 w-8 mobile:h-7 mobile:w-7' />
-            <Skeleton sizeClassName="w-56 mobile:w-48 h-8 mobile:h-7" />
+            <Skeleton sizeClassName="w-56 mobile:w-48 h-6 mobile:h-5" />
           </div>
         </div>
       </div>
@@ -51,8 +52,10 @@ const PostDetailSkeleton = () => {
           <Skeleton sizeClassName="w-[calc(100%-110px)] h-7 mobile:h-5 mr-5 mobile:mr-0" />
         </div>
       </div>
-        <div>
-          <Skeleton sizeClassName="w-56 mobile:w-48 h-8 mobile:h-7" />
+        <div className='min-h-[250px] mobile:min-h-[150px] flex flex-col justify-center'>
+          <div className="text-2xl font-bold text-black100 mobile:text-xl">
+            프로젝트 소개
+          </div>
           <div className="py-10 mobile:py-5 border-t-2 border-b-2 mt-5 mobile:mt-3">
             <Skeleton sizeClassName="w-full h-40 mobile:h-32" />
           </div>
