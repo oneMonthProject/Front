3d84b8ae28c532eef1cf291cff48d5e913fcdb11
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-10-02 16:43:59 +0900

    design: 프로젝트 상세 > 알림 > 투표완료시 radio input disabled로 보이도록 수정

--- src/components/ui/votebar/VoteBar.tsx
+++ src/components/ui/votebar/VoteBar.tsx
@@ -29,7 +29,8 @@ function VoteBar({label, counts, barColor, maxCounts, onChangeVoteHandler, disab
 
     return (
         <div className='flex mobile:flex-col justify-center items-center mobile:items-start'>
-            <div className='text-nowrap tablet:basis-[50px] text-[18px] mobile:text-base text-greyDarkblue font-medium'>{label}</div>
+            <div
+                className='text-nowrap tablet:basis-[50px] text-[18px] mobile:text-base text-greyDarkblue font-medium'>{label}</div>
             <div className='flex w-full justify-around items-center space-x-12 mobile:space-x-9'>
                 <div
                     className={`relative tablet:basis-[70%] mobile:w-full h-2 ${barBackgroundColorColor} rounded-full`}>
@@ -41,16 +42,14 @@ function VoteBar({label, counts, barColor, maxCounts, onChangeVoteHandler, disab
                         className='absolute top-[-7px] left-[102%] mobile:text-sm text-greyBlue font-medium'>{`${counts}/${maxCounts}`}</span>
                 </div>
                 <div className='flex space-x-2 h-5 items-center'>
-                    {
-                        !disabled &&
-                        <input
-                            type="radio" name='voteOption' id='voteOption_agree'
-                            onChange={onChangeCheckedHandler}
-                            value={value}
-                            checked={checked}
-                            className='self-stretch border-gray-400 text-indigo-600 focus:ring-none'
-                        />
-                    }
+                    <input
+                        type="radio" name='voteOption' id='voteOption_agree'
+                        onChange={onChangeCheckedHandler}
+                        value={value}
+                        checked={checked}
+                        className='self-stretch border-gray-400 text-indigo-600 focus:ring-none'
+                        disabled={disabled}
+                    />
                 </div>
             </div>
         </div>
