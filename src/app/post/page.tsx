5ff872935797078298aaf050a478a602a29c4b85
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-10-11 20:33:18 +0900

    feat: 팀프로젝트 게시글 상세페이지에 SEO 키워드 넣은 메타데이터 추가

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,8 +1,31 @@
 import React from "react";
 import PostDetail from "@/components/postDetail/PostDetail";
 import ConfirmModal from "@/components/ui/ConfirmModal";
+import {Metadata} from "next";
+import {numStrToBigInt} from "@/utils/common";
+import {getPost} from "@/service/post/post";
+import {PostInfo, ResponseBody} from "@/utils/type";
 
-const PostDetailPage = ({searchParams: {postId, projectId}}: { searchParams: { postId: string, projectId:string } }) => {
+
+export async function generateMetadata({
+                                           searchParams: {
+                                               postId
+                                           }
+                                       }: { searchParams: { postId: string } }): Promise<Metadata> {
+    const postInfo: ResponseBody<PostInfo> = await getPost(numStrToBigInt(postId));
+
+    return {
+        title: `${postInfo.data!.title} - TRUSTCREWS | 책임감 있는 사이드 프로젝트 팀, 팀원을 구하는 방법`,
+        description: `${postInfo.data!.content}`
+    }
+}
+
+const PostDetailPage = ({
+                            searchParams: {
+                                postId,
+                                projectId
+                            }
+                        }: { searchParams: { postId: string, projectId: string } }) => {
     return (
         <>
             <PostDetail postId={postId} projectId={projectId}/>
